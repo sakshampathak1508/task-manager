f2f4907bc0f1c71d93f1661d710499ff0b8ac074
saksham <sakshamvpathak@gmail.com>
2022-07-13 01:36:12 +0530

    new

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include,url
+from django.urls import path,include,re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve as mediaserve
@@ -28,5 +28,5 @@ urlpatterns = [
 #     urlpatterns += static(settings.MEDIA_URL,
 #                         document_root=settings.MEDIA_ROOT)
 # else:
-urlpatterns.append(url(f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$',
+urlpatterns.append(re_path(f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$',
                     mediaserve, {'document_root': settings.MEDIA_ROOT}))
