053f04d9ed3f35b9bd5415ae62b7b27753dedd68
saksham <sakshamvpathak@gmail.com>
2022-07-13 01:17:06 +0530

    iniss

--- frontend/views.py
+++ frontend/views.py
@@ -10,18 +10,18 @@ from django.contrib.auth import authenticate,login,logout
 # Create your views here.
 
 def index(request):
-    data = requests.get('http://localhost:8000/api/all-projects/').json()
+    data = requests.get('https://task-manager-ufaber.herokuapp.com/api/all-projects/').json()
     return render(request,"frontend/index.html",{'data':data})
 
 def project(request,pk):
-    data = requests.get('http://localhost:8000/api/project/'+str(pk)).json()
+    data = requests.get('https://task-manager-ufaber.herokuapp.com/api/project/'+str(pk)).json()
     params ={'pid':pk}
-    task_data = requests.get('http://localhost:8000/api/task/',params=params).json()
+    task_data = requests.get('https://task-manager-ufaber.herokuapp.com/api/task/',params=params).json()
     return render(request,"frontend/project.html",{'data':data,'task_data':task_data})
 
 def task(request,pk1,pk2):
     # params ={'pid':pk2}
-    data = requests.get('http://localhost:8000/api/task/'+str(pk2)).json()
+    data = requests.get('https://task-manager-ufaber.herokuapp.com/api/task/'+str(pk2)).json()
     return render(request,"frontend/tasks.html",{'data':data})
 
 def task_edit(request,pk1,pk2):
@@ -41,12 +41,12 @@ def task_edit(request,pk1,pk2):
             data['start_date'] = sd
         if(ed!=''):
             data['end_date'] = ed
-        r = requests.put('http://localhost:8000/api/task/'+str(pk2)+"/",data=data)
+        r = requests.put('https://task-manager-ufaber.herokuapp.com/api/task/'+str(pk2)+"/",data=data)
         return redirect('/project/'+str(pk1)+'/task/'+str(pk2))
     return render(request,"frontend/task_form.html",{'data':data})
 
 def task_delete(request,pk1,pk2):
-    data = requests.delete('http://localhost:8000/api/task/'+str(pk2))
+    data = requests.delete('https://task-manager-ufaber.herokuapp.com/api/task/'+str(pk2))
     return redirect('/project/'+str(pk1))
 
 
@@ -67,7 +67,7 @@ def task_create(request,pk):
             data['start_date'] = sd
         if(ed!=''):
             data['end_date'] = ed
-        r = requests.post('http://localhost:8000/api/task-add/',data=data)
+        r = requests.post('https://task-manager-ufaber.herokuapp.com/api/task-add/',data=data)
         return redirect('/project/'+str(pk))
     return render(request,"frontend/task_create.html",{'data':data})
 
@@ -86,7 +86,7 @@ def project_create(request):
             data['start_date'] = sd
         if(ed!=''):
             data['end_date'] = ed
-        r = requests.post('http://localhost:8000/api/project/',data=data)
+        r = requests.post('https://task-manager-ufaber.herokuapp.com/api/project/',data=data)
         image = request.FILES.get('img',False)
         r = r.json()
         pk = r['id']
@@ -112,7 +112,7 @@ def project_edit(request,pk):
             data['start_date'] = sd
         if(ed!=''):
             data['end_date'] = ed
-        r = requests.put('http://localhost:8000/api/project/'+str(pk)+"/",data=data)
+        r = requests.put('https://task-manager-ufaber.herokuapp.com/api/project/'+str(pk)+"/",data=data)
         image = request.FILES.get('img',False)
         if(image):
             obj = Project.objects.filter(id=pk).first()
@@ -122,5 +122,5 @@ def project_edit(request,pk):
     return render(request,"frontend/project_form.html",{'data':data})
 
 def project_delete(request,pk):
-    data = requests.delete('http://localhost:8000/api/project/'+str(pk))
+    data = requests.delete('https://task-manager-ufaber.herokuapp.com/api/project/'+str(pk))
     return redirect('/')
