e1680c2de9f3e5f06330ee1da1f1e2188c009385
saksham <sakshamvpathak@gmail.com>
2022-07-13 01:31:30 +0530

    inissss

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -17,13 +17,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
+from django.views.static import serve as mediaserve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('backend.urls')),
     path('',include('frontend.urls')),
-    # url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
 ]
-
-urlpatterns += static(settings.MEDIA_URL,
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
                         document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns.append(url(f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$',
+                       mediaserve, {'document_root': settings.MEDIA_ROOT}))
