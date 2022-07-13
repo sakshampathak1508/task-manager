0bc1255b3b24d737eebbcc6dcf2dec48e7ee1de5
saksham <sakshamvpathak@gmail.com>
2022-07-14 01:15:20 +0530

    yo

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include,re_path
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.views.static import serve as mediaserve
 
@@ -28,5 +29,7 @@ urlpatterns = [
 #     urlpatterns += static(settings.MEDIA_URL,
 #                         document_root=settings.MEDIA_ROOT)
 # else:
-urlpatterns.append(re_path(f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$',
-                    mediaserve, {'document_root': settings.MEDIA_ROOT}))
+urlpatterns.append(url(f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$',
+                       mediaserve, {'document_root': settings.MEDIA_ROOT}))
+urlpatterns.append(url(f'^{settings.STATIC_URL.lstrip("/")}(?P<path>.*)$',
+                       mediaserve, {'document_root': settings.STATIC_ROOT}))
